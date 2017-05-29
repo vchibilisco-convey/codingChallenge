f671d884fb09066f0f139a7cc79b6885b1e9f611
Gonzalo Mercado <gonzalo.mercado@sovos.com>
2017-05-29 13:55:39 -0300

    Adding upsert

--- webService/models/jobs.model.js
+++ webService/models/jobs.model.js
@@ -1,12 +1,18 @@
+var statuses = require('../constant/statuses');
 var Job = require('../classes/job');
 
 exports.create = function(pJob, cb) {
-  var job = new Job();
-  job.name = pJob.name;
-  job.status = pJob.status;
-  job.teams = ['Dolphin', 'Blowfish'];
+  var query = {
+    name: pJob.name
+  };
 
-  job.save(function(err, res){
+  var job = {
+    status: pJob.status,
+    lastStatus: pJob.lastStatus, //This will be returned from mysql attool.
+    teams: ['Blowfish', 'Stonefish']
+  }
+
+  Job.findOneAndUpdate(query, job, {upsert: true}, function(err, res){
     cb(err, res);
   });
 };
@@ -17,7 +23,7 @@ exports.findAll = function(cb){
   });
 };
 
-exports.findByName = function(pjob, cb){
+exports.findByName = function(pJob, cb){
   Job.find({'name': pJob.name}, function(err, job){
     cb(err, job);
   });
