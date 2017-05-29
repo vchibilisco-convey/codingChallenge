81b2b67466d171b8a50a3d5e723399f37069466a
vchibilisco <vchibilisco@convey.com>
2017-05-29 10:23:08 -0500

    filtro por team

--- daemon/models/jobs.model.js
+++ daemon/models/jobs.model.js
@@ -1,4 +1,5 @@
 var Job = require('../classes/job');
+var Configuration = require('../config/config');
 
 exports.create = function(pJob, cb) {
   var job = new Job();
@@ -6,13 +7,19 @@ exports.create = function(pJob, cb) {
   job.status = pJob.status;
   job.teams = ['Blowfish'];
 
-  job.save(function(err, res){
+  job.save(function(err, res) {
     cb(err, res);
   });
-}
+};
 
-exports.findAll = function(cb){
-  Job.find(function(err, jobs){
+exports.findAll = function(cb) {
+  Job.find(function(err, jobs) {
     cb(err, jobs);
   });
 };
+
+exports.findByTeam = function(cb) {
+  Job.find({'teams': Configuration.team}, function(err, jobs) {
+    cb(err, jobs);
+  })
+}
